05549b24f9f7b53e8ea6033b3e237c64faded354
Orlando Mata Monge <orl1289@gmail.com>
2024-05-01 18:39:27 +0200

    Fixed the NavBar, and modified Tailwind config for screen size responsiveness

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { Button } from "./ui/button";
 import { HamburgerIcon } from "@chakra-ui/icons";
 
-
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -12,11 +11,11 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className= "p-4">
+    <nav className="p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white">Logo</div>
-        <Button className="block md:hidden text-white" onClick={toggleMenu}>
-          {/* Hamburger Icon */}
+        <Button className="block laptop:hidden text-white" onClick={toggleMenu}>
+          {/* Setting the Hamburger Icon */}
           {isOpen ? (
             <HamburgerIcon></HamburgerIcon>
           ) : (
@@ -25,7 +24,8 @@ const Navbar: React.FC = () => {
         </Button>
         <div
           className={`${
-            isOpen ? "block" : "hidden"} md:block md:flex md:items-center`}
+            isOpen ? "block" : "hidden"
+          } laptop:block laptop:flex laptop:items-center`}
         >
           <Button className="text-white mr-4">Home</Button>
           <Button className="text-white mr-4">About</Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,21 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+    screens: {
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
     container: {
       center: true,
       padding: "2rem",
       screens: {
-        tablet: "640px",
-        // => @media (min-width: 640px) { ... }
-
-        laptop: "1024px",
-        // => @media (min-width: 1024px) { ... }
-
-        desktop: "1280px",
-        // => @media (min-width: 1280px) { ... }
+        "2xl": "1400px",
       },
     },
     extend: {
