e5dc8e02fb705597f84b02baecfec94e751ce5a8
Orlando Mata Monge <orl1289@gmail.com>
2026-07-17 20:09:51 +0200

    Final changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import Carousel from "./components/Carousel";
+
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <>
       <NavBar></NavBar>
       <div className=" laptop: w-auto flex items-center justify-center p-6">
-        <Carousel></Carousel>
+       
       </div>
 
       <div>This is my new app</div>
